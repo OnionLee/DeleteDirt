1276a42b638cd8ac5a2aac5b8d87f321c59d714b
Park June Chul <pjc0247@naver.com>
2014-03-19 15:04:41 +0900

    필요없는 메소드 제거

--- Class/MaskImage.cpp
+++ Class/MaskImage.cpp
@@ -10,18 +10,6 @@ MaskImage::~MaskImage()
 {
 }
 
-MaskImage * MaskImage::create()
-{
-	MaskImage *ptr = new MaskImage();
-	if (ptr && ptr->init())
-	{
-		ptr->autorelease();
-		return ptr;
-	}
-	CC_SAFE_DELETE(ptr);
-	return nullptr;
-}
-
 MaskImage * MaskImage::create(const char * Image, const char * mask)
 {
 	MaskImage *ptr = new MaskImage();
@@ -34,17 +22,12 @@ MaskImage * MaskImage::create(const char * Image, const char * mask)
 	return nullptr;
 }
 
-bool MaskImage::init()
-{
-	return true;
-}
-
 bool MaskImage::initWithFile(const char * Image, const char * mask)
 {
 	if (!init())
 		return false;
 
-	//Áö¿ï ÀÌ¹ÌÁö
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½Ì¹ï¿½ï¿½ï¿½
 	m_pImage = Sprite::create(Image);
 	m_pImage->retain();
 	m_pImage->setAnchorPoint(ccp(0, 0));
@@ -52,20 +35,20 @@ bool MaskImage::initWithFile(const char * Image, const char * mask)
 	setPosition(ccp(m_pImage->getContentSize().width / 2,
 		m_pImage->getContentSize().height / 2));
 
-	//¸¶½ºÅ©(ºí·»µù)
+	//ï¿½ï¿½ï¿½ï¿½Å©(ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½)
 	m_pMask = Sprite::create(mask);
 	m_pMask->retain();
 	ccBlendFunc blend1 = { GL_ZERO, GL_ONE_MINUS_SRC_ALPHA };
 	m_pMask->setBlendFunc(blend1);
 
-	//·»´õ ÅØ½ºÃÄ(ºí·»µù)
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½Ø½ï¿½ï¿½ï¿½(ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½)
 	m_pRender = RenderTexture::create(m_pImage->getContentSize().width,
 									m_pImage->getContentSize().height);
 	ccBlendFunc blend2 = { GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA };
 	m_pRender->getSprite()->setBlendFunc(blend2);
 	addChild(m_pRender, 2);
 
-	//ÀÌ¹ÌÁö ÇÑ¹ø ·»´õ ÇÔ
+	//ï¿½Ì¹ï¿½ï¿½ï¿½ ï¿½Ñ¹ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½
 	m_pRender->begin();
 	m_pImage->visit();
 	m_pRender->end();
